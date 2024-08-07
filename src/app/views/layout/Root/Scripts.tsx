b67b3c40fac6baa9ebe6ce74683bc12215702924
Hannibal B. Moulvad <hbm@impact.dk>
2024-08-07 13:25:55 +0200

    feat: Add HTMX library for dynamic HTML updates

--- src/app/views/layout/Root/Scripts.tsx
+++ src/app/views/layout/Root/Scripts.tsx
@@ -6,6 +6,12 @@ export default function Scripts() {
         defer
         src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"
       ></script>
+      {/* HTMX */}
+      <script
+        src="https://unpkg.com/htmx.org@2.0.1"
+        integrity="sha384-QWGpdj554B4ETpJJC9z+ZHJcA/i59TyjxEPXiiUgN2WmTyV5OEZWCD6gQhgkdpB/"
+        crossorigin="anonymous"
+      ></script>
     </>
   );
 }
