4c952edb9b83afbc125b31ba2df7fb69eca07cef
Hannibal B. Moulvad <hbm@impact.dk>
2024-09-03 21:28:47 +0200

    refactor: Update UI component imports and file structure

--- src/api/routes.tsx
+++ src/api/routes.tsx
@@ -1,14 +1,9 @@
 import { Hono } from "hono";
-import userRoutes from "./users";
-import battlenetRoutes from "./battlenet";
 
 const apiRoutes = new Hono();
 
-apiRoutes
-  .get("/", (c) => {
-    return c.html("Hello, World!");
-  })
-  .route("/battlenet", battlenetRoutes)
-  .route("/users", userRoutes);
+apiRoutes.get("/", (c) => {
+  return c.html("Hello, World!");
+});
 
 export default apiRoutes;
--- src/types/databaseTypes.ts
+++
@@ -1,9 +0,0 @@
-export namespace DatabaseTypes {
-  export type User = {
-    id: number;
-    name: string;
-    email: string;
-  };
-
-  export type Token = { type: "battlenet"; token: string; expires: number };
-}
